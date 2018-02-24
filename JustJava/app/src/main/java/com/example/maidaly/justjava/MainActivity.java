75983ee8d181dc50721a3357a95addbee7b764f1
maidaly <noor.daly6@gmail.com>
2018-02-24 10:12:38 +0200

    Version 1

---
+++ JustJava/app/src/main/java/com/example/maidaly/justjava/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.maidaly.justjava;
+
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ArrayAdapter;
+        import android.widget.TextView;
+        import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    int quantity=0;
+    public void submitOrder(View view) {
+        Intent intent = new Intent(this,MakeOrder.class); // attach two activities
+        startActivity(intent);
+//        int price = quantity*5;
+//        String priceMessage="Total= "+ price + '$' +"\nThank You!";
+//        displayMessage(priceMessage);
+    }
+    public void increment(View view){
+       quantity = quantity + 1;
+        display(quantity);
+    }
+    public void decrement(View view){
+        quantity = quantity - 1;
+        display(quantity);
+    }
+    public void new_order(View view){
+        quantity = 0;
+        display(quantity);
+        displayPrice(0);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
---
+++ JustJava/app/src/main/java/com/example/maidaly/justjava/MakeOrder.java
@@ -0,0 +1,13 @@
+package com.example.maidaly.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MakeOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.make_order);
+    }
+}
