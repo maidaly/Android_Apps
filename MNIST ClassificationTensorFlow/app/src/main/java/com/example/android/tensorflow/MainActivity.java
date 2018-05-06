938c61bc2def075c3e25db32b0c3b58c60feb652
maidaly <noor.daly6@gmail.com>
2018-05-06 05:42:26 +0200

    updated files

--- MNIST ClassificationTensorFlow/app/src/main/java/com/example/android/tensorflow/MainActivity.java
+++ MNIST ClassificationTensorFlow/app/src/main/java/com/example/android/tensorflow/MainActivity.java
@@ -139,7 +139,7 @@ public class MainActivity extends Activity implements View.OnClickListener, View
                     text += classifier.name() + ": ?\n";
                 } else {
                     //else output its name
-                    text += String.format("%s:\n Number: %s,  Probability: %f\n", classifier.name(), res.getLabel(),
+                    text += String.format("%s:\n Number: %s,  Probability: %f\n\n", classifier.name(), res.getLabel(),
                             res.getConf());
                 }
             }
